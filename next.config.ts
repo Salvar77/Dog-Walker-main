3eaa3468d6233aea5e0ebd66ddc31ef6e7fa4ed7
Łukasz <lukaszkus77@gmail.com>
2025-07-05 15:41:08 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextI18NextConfig = require("./next-i18next.config.js");
 module.exports = {
   reactStrictMode: true,
   i18n: nextI18NextConfig.i18n,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
